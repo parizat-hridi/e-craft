39d0fe9ec96766c50a25a9a7afa2524dce04ac9b
parizat-hridi <parizatbintekabir@gmail.com>
2022-04-22 16:44:59 +0600

    latest

--- pages/index.js
+++ pages/index.js
@@ -3,30 +3,32 @@ import Nav from './components/nav';
 import Image from 'next/image';
 
 export default function Home({ products }) {
-  console.log({ products });
+  console.log(supabase.auth.user());
 
   return (
     <>
       <Nav />
-      <div className="h-auto text-white bg-gray-800 flex pt-8 grid grid-cols-4 gap-4">
-        {products.map(product => (
-          <>
-            <div className="mb-5 ml-5">
-              <Image
-                src={product.image}
-                width={250}
-                height={250}
-                alt="picture"
-              />
-              <h1 key={product.id}>Product: {product.name}</h1>
-              <p>Price: ${product.price}</p>
-              <p>Seller Email: {product.seller_email}</p>
-              <button className="mt-2 text-lg text-white font-semibold bg-green-500 py-2 px-2 rounded-md focus:outline-none focus:ring-2">
-                Buy Now
-              </button>
-            </div>
-          </>
-        ))}
+      <div className="h-screen bg-gray-800">
+        <div className="h-auto text-white bg-gray-800 flex pt-8 grid grid-cols-4 gap-4">
+          {products.map(product => (
+            <>
+              <div className="mb-5 ml-5">
+                <Image
+                  src={product.image}
+                  width={250}
+                  height={250}
+                  alt="picture"
+                />
+                <h1 key={product.id}>Product: {product.name}</h1>
+                <p>Price: ${product.price}</p>
+                <p>Seller Email: {product.seller_email}</p>
+                <button className="mt-2 text-lg text-white font-semibold bg-green-500 py-2 px-2 rounded-md focus:outline-none focus:ring-2">
+                  Buy Now
+                </button>
+              </div>
+            </>
+          ))}
+        </div>
       </div>
     </>
   );
